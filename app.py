149bdfdb62a33015acb37ea1f7601bed00fec596
nsakwa <nsakwa@gmail.com>
2025-05-30 22:44:31 +0300

    sklearnimport

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import pandas as pd
 import os
 import re
 import warnings
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import HisGradientBoostingRegressor
+from flask import Flask, request, render_template, jsonify
+import joblib
 
-train=pd.read_csv("/kaggle/input/global-unemployment-data/global_unemployment_data.csv")
-train
+app = Flask(__name__)
+
+try:
+    train_df=pd.read_csv("/kaggle/input/global-unemployment-data/global_unemployment_data.csv")
+except FileNotFoundError:
+    train_df=pd.read_csv("global_unemployement_data.csv")
+
